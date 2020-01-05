a67590a8dc596cc3e8798ef159295172671a91f6
khWebDev <khWebDev@users.noreply.github.com>
2020-01-05 14:57:58 +0000

    amend url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const RandomQuote_API_URL = 'http://api.quotable.io/random'
+const RandomQuote_API_URL = 'https://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
